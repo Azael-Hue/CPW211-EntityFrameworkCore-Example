70d4ba365f67688d2f7d9a56cb0c7693de1e86d2
Azael H. <166276745+Azael-Hue@users.noreply.github.com>
2024-07-24 23:19:17 -0700

    Added test code for create and select

--- EntityFrameworkCore/Program.cs
+++ EntityFrameworkCore/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using EntityFrameworkCore;
+
+StudentContext dbContext = new();
+
+// Add with EF Core
+Student s = new()
+{
+    FullName = "John Doe",
+    EmailAddress = "asdf123@mail.com",
+    DateOfBirth = new DateTime(1990, 1, 1)
+};
+
+Student s2 = new()
+{
+    FullName = "Jane Doe",
+    EmailAddress = "qwerty123@mail.com",
+    DateOfBirth = new DateTime(2000, 1, 1)
+};
+
+dbContext.Students.Add(s); // Prepares the student insert statement
+dbContext.SaveChanges(); // Executes pending insert, (In Actuality: Execute any pending insert/update/delete)
+
+dbContext.Students.Add(s2);
+dbContext.SaveChanges();
+
+// Retrieve Students from DB
+List<Student> allStudents = dbContext.Students.ToList(); // Method syntax
+allStudents = (from stu in dbContext.Students
+               select stu).ToList(); // Query syntax
+foreach (Student stu in allStudents)
+{
+    Console.WriteLine($"{stu.FullName} has an email of {stu.EmailAddress}");
+    Console.WriteLine();
+}
